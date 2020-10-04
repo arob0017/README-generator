5bf46f3e7047f68e3687116775d44a5bd6b5f1af
Simone Cheah <simone_cheah@hotmail.com>
2020-10-05 05:07:37 +1100

    ✨feature:  add function to write to file

--- index.js
+++ index.js
@@ -1,15 +1,80 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+// const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown.js")
+
+// writeFileAsync = util.promisify(fs.writeFile);
+
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "title",
+        message: "Title:"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Description:"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Installation:"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Usage:"
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "License:"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Contributing:"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Tests:"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Questions:"
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err, data) => {
+        return err ? err : data
+    })
 }
 
 // function to initialize program
-function init() {
+async function init() {
+    console.log(
+`--------------------------------------------------------
+THE PROFESSIONAL README GENERATOR
 
+Please input info about your README info at the prompts 
+to create your own professional README.md!
+--------------------------------------------------------`);
+
+    try {
+        // const userInput = await inquirer.prompt( questions );
+        // console.log(userInput);
+        // const readmeData = generateMarkdown( answers );
+
+        writeToFile("testREADME.md", "readmeData")
+    } catch {
+        console.log(`Error: something is wrong`)
+    }
 }
 
 // function call to initialize program
