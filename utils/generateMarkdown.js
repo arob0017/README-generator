2957cc559680089d249fe226c7289d6749e7fef3
Simone Cheah <simone_cheah@hotmail.com>
2020-10-05 05:31:12 +1100

    🖥️wip:  added some structure to generate md

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,18 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const { title, description, installation, usage, license, contributing, tests, questions} = data;
+  return `
+# ${title} 
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
 
+## Description 
+
+## Table of Contents (Optional)
+## Installation
+## Usage 
+## Credits
+## License
+## FAQ
 `;
 }
 
